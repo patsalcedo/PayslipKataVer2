7d3d7df4754851b70e901bcedb7b44afce7d623b
patsalcedo <patricia.salcedo15@outlook.com>
2020-12-02 10:30:30 +1300

    added calcualteincometax method and test (works)

--- PayslipVer2.Tests/UnitTest1.cs
+++ PayslipVer2.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace PayslipVer2.Tests
     {
         [Theory]
         [InlineData(60050, 5004)]
-        [InlineData(120000,10000)]
+        [InlineData(120000, 10000)]
         public void Gross_Income_Calculation_Test(int annualSalary, int expectedOutput)
         {
             // GIVEN
@@ -19,10 +19,24 @@ namespace PayslipVer2.Tests
             // THEN
             Assert.Equal(expectedOutput, actual);
         }
+
+        [Fact]
+        public void Income_Tax_Calculation_Tax()
+        {
+            // GIVEN
+            var payslipCalculator = new PayslipCalculator();
+            
+            // WHEN
+            var actual = payslipCalculator
+                .CalculateIncomeTax(3572, 60050, 37000, 0.325);
+
+            // THEN
+            Assert.Equal(922, actual);
+        }
         
         [Theory]
         [InlineData(5004, 922, 4082)]
-        [InlineData(10000,2669,7331)]
+        [InlineData(10000, 2669, 7331)]
         public void Net_Income_Calculation_Test(int grossIncome, int incomeTax, int expectedOutcome)
         {
             // GIVEN
@@ -37,7 +51,7 @@ namespace PayslipVer2.Tests
         
         [Theory]
         [InlineData(5004, 9, 450)]
-        [InlineData(10000,10,1000)]
+        [InlineData(10000, 10, 1000)]
         public void Super_Calculation_Test(int grossIncome, int superRate, int expectedOutcome)
         {
             // GIVEN
@@ -49,5 +63,17 @@ namespace PayslipVer2.Tests
             // THEN
             Assert.Equal(expectedOutcome, actual);
         }
+        
+        // [Fact]
+        // public void Pay_Period_Calculation_Test()
+        // {
+        //     
+        // }
+        
+        // [Fact]
+        // public void Throw_Exception_For_Negatives()
+        // {
+        //     
+        // }
     }
 }
--- PayslipVer2/PayslipCalculator.cs
+++ PayslipVer2/PayslipCalculator.cs
@@ -4,17 +4,28 @@ namespace PayslipVer2
 {
     public class PayslipCalculator
     {
+        private const int AnnualMonths = 12;
+
         public int CalculateGrossIncome(double annualSalary)
         {
-            var sum = (int) Math.Round(annualSalary / 12);
+            var sum = (int) Math.Round(annualSalary / AnnualMonths);
             return sum;
+        }
+
+        public int CalculateIncomeTax(int taxOnIncome, int annualSalary, int lowerTaxableIncomeBracket, double taxExtra)
+        {
+            // might have to have an IncomeTax object as parameter containing taxOnIncome, the tax brackets and taxExtra
+            // need better name for taxExtra
+            
+            var incomeTax = (int) Math.Round((taxOnIncome + (annualSalary - lowerTaxableIncomeBracket) * taxExtra) / AnnualMonths);
+            return incomeTax;
         }
 
         public int CalculateNetIncome(int grossIncome, int incomeTax)
         {
             return grossIncome - incomeTax;
         }
-        
+
         public int CalculateSuper(int grossIncome, int superRate)
         {
             var super = (int) Math.Round(grossIncome * superRate * 0.01);
